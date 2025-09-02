98b781256b1b67270ccf1b2e8dc0d9c0bdfdda84
Avinash Dhanuka <avunashdhanuka@gmail.com>
2025-09-02 23:56:58 +0530

    CRUD Operation In Model | Database

--- core/home/models.py
+++ core/home/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 # Create your models here.
 
+
+#crud operations
+ 
 class Student(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField()
     email = models.EmailField()
     address = models.TextField(blank=True, null=True)
 
-class Product(models.Model):
-    pass
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=50)
+    speed = models.IntegerField(default = 50)
+
+    def __str__(self):
+        return self.car_name
+
